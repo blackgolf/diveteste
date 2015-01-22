215d43c4720620b9698dcf3621337b50d1372401
Thien Nguyen <nguyenphongthien@yahoo.com>
2015-01-23 02:21:47 +0700

    update

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 angular.module('conference.config', [])
 
-    .constant('FB_APP_ID', '932536800093017') // Use your own FB app id
+.constant('FB_APP_ID', '932536800093017') // Use your own FB app id
 
-    .constant('SERVER_PATH','http://localhost:5000');
-//    .constant('SERVER_PATH','http://ioconf.herokuapp.com');
+// .constant('SERVER_PATH','http://localhost:5000');
+.constant('SERVER_PATH', 'http://128.199.146.12:5000');
