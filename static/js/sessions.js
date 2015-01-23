cabe7ccae7172c42194e37553ccf923eee116102
Thien Nguyen <nguyenphongthien@yahoo.com>
2015-01-23 23:38:21 +0700

    update

--- static/js/sessions.js
+++ static/js/sessions.js
@@ -42,11 +42,10 @@ angular.module('conference.sessions', ['ngResource', 'conference.config', 'confe
 	});
 	$scope.toolbarheight = 95;
 	$scope.changeSession = function (session) {
-		if (!$scope.noDetail) {
+		if ($scope.detailSession === session && $scope.firstChange) {
 			$scope.noDetail = true;
 			$scope.footerHeight = 0;
 			$scope.plusBottom = '22px';
-			// $scope.showFooter(true);
 		} else {
 			$scope.noDetail = false;
 			$scope.detailSession = session;
@@ -55,38 +54,29 @@ angular.module('conference.sessions', ['ngResource', 'conference.config', 'confe
 			$scope.firstChange = true;
 		}
 	};
-	// $scope.showFooter = function (async) {
-	// 	if ($scope.noDetail && $scope.detailSession) {
-	// 		var top = $ionicScrollDelegate.getScrollPosition().top;
-	// 		$scope.bottomPosition = $('ion-content>.scroll').height() - $('ion-content').height();
-	// 		if (top > $scope.bottomPosition - 95) {
-	// 			$scope.footerHeight = (top - $scope.bottomPosition + 95) * 3;
-	// 		} else {
-	// 			$scope.footerHeight = 0;
-	// 		}
-	// 		if ($scope.footerHeight > 50) {
-	// 			$scope.plusBottom = ($scope.footerHeight - 28) + 'px';
-	// 		} else {
-	// 			$scope.plusBottom = '22px';
-	// 		}
-	// 		$scope.footerHeight += 'px';
-	// 		if (!async) {
-	// 			$scope.$apply();
-	// 		}
-	// 	}
-	// };
 
 	var startY, endY, moveY, now, down = false,
+		move = [],
 		calculateSwipe = function () {
-			var gap = endY - startY;
-			var timegap = new Date() - now;
-			if (timegap > 0) {
-				if (gap / timegap > 0.01) {
-					swipe(1);
-				} else {
-					swipe(0);
-				};
+			for (i = 0; i < move.length; i++) {
+				if (move[move.length - 1].t - move[i].t < 500) {
+					var gap = move[move.length - 1].y - move[i].y;
+					var timegap = move[move.length - 1].t - move[i].t;
+					if (timegap > 0) {
+						if (gap / timegap > 0.05) {
+							swipe(1);
+						} else {
+							swipe(0);
+						};
+					}
+					break;
+				}
 			}
+			down = false;
+			startY = undefined;
+			move = [];
+			$('#toolbar').removeClass('notransition');
+			$('.floatingContainer').removeClass('notransition');
 		},
 		swipe = function (type) {
 			switch (type) {
@@ -127,34 +117,42 @@ angular.module('conference.sessions', ['ngResource', 'conference.config', 'confe
 		});
 
 		$('#toolbar').on('touchstart', function (e) {
-			if (!down) {
-				startY = e.originalEvent.touches[0].clientY;
-				moveY = startY;
-				down = true;
-				now = new Date();
-			}
+			down = true;
+			$(this).addClass('notransition');
+			$('.floatingContainer').addClass('notransition');
 		});
 
 		$('#toolbar').on('mousedown', function (e) {
-			if (!down) {
-				startY = e.originalEvent.clientY;
-				moveY = startY;
-				down = true;
-				now = new Date();
-			}
+			down = true;
+			$('#toolbar').addClass('notransition');
+			$('.floatingContainer').addClass('notransition');
 		});
 
-		$('#toolbar').on('touchmove', function (e) {
+		$('ion-pane').on('touchmove', function (e) {
 			if (down) {
 				moveY = e.originalEvent.changedTouches[0].clientY;
+				if (startY === undefined) {
+					startY = moveY;
+				}
+				move.push({
+					y: moveY,
+					t: e.timeStamp
+				});
 				now = new Date();
 				toolbarMove();
 			}
 		});
 
-		$('#toolbar').on('mousemove', function (e) {
+		$('ion-pane').on('mousemove', function (e) {
 			if (down) {
 				moveY = e.originalEvent.clientY;
+				if (startY === undefined) {
+					startY = moveY;
+				}
+				move.push({
+					y: moveY,
+					t: e.timeStamp
+				});
 				now = new Date();
 				toolbarMove();
 			}
@@ -164,7 +162,6 @@ angular.module('conference.sessions', ['ngResource', 'conference.config', 'confe
 			if (down) {
 				endY = e.originalEvent.changedTouches[0].clientY;
 				calculateSwipe();
-				down = false;
 			}
 		});
 
@@ -172,7 +169,6 @@ angular.module('conference.sessions', ['ngResource', 'conference.config', 'confe
 			if (down) {
 				endY = e.originalEvent.clientY;
 				calculateSwipe();
-				down = false;
 			}
 		});
 	})
