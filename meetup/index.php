b8bb3e527a8ca63caf917a570cb09976ac9bae7c
Thien Nguyen <nguyenphongthien@yahoo.com>
2015-01-27 02:05:20 +0700

    update

--- meetup/index.php
+++ meetup/index.php
@@ -2,8 +2,10 @@
 // ini_set('display_errors', 1);
 // error_reporting(~0);
 header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: true"); 
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS'); 
-header('Access-Control-Allow-Headers: txt/html');  
+header('Access-Control-Allow-Headers: X-Requested-With');
+header('Access-Control-Allow-Headers: Content-Type');
 
 require('lib.php');
 if(!isset($_GET['code'])){
