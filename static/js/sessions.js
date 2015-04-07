dac892fe482bedf028407114df76c5fc1964f715
Thien Nguyen <nguyenphongthien@gmail.com>
2015-04-08 00:20:42 +0700

    update

--- static/js/sessions.js
+++ static/js/sessions.js
@@ -47,6 +47,7 @@ angular.module('conference.sessions', ['ngResource', 'conference.config', 'confe
                             var bgImg = getImage(session.description);
                             if (bgImg) {
                                 session.photo_url = getImage(session.description);
+                                session.backgroundColor = 'rgba(0,0,0,0.4)';
                             } else {
                                 session.backgroundColor = randomColor(0, index % 3);
                             }
